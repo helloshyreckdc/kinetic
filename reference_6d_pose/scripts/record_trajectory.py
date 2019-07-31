946b09dc7f490cbb0425acbe7428d2bc611fa1df
shyreckdc <shyreckdc@gmail.com>
2019-07-31 16:40:20 +0800

    test clion git

--- reference_6d_pose/scripts/record_trajectory.py
+++ reference_6d_pose/scripts/record_trajectory.py
@@ -33,23 +33,28 @@
 #
 # Revision $Id$
 
-# record trajectory
+## Simple talker demo that listens to std_msgs/Strings published 
+## to the 'chatter' topic
 
 import rospy
 from std_msgs.msg import String
 
-def record_trajectory():
-    pub = rospy.Publisher('chatter', String, queue_size=10)
+def callback(data):
+    rospy.loginfo(rospy.get_caller_id() + 'I heard %s', data.data)
+
+def listener():
+
+    # In ROS, nodes are uniquely named. If two nodes with the same
+    # name are launched, the previous one is kicked off. The
+    # anonymous=True flag means that rospy will choose a unique
+    # name for our 'listener' node so that multiple listeners can
+    # run simultaneously.
     rospy.init_node('record_trajectory', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
-    while not rospy.is_shutdown():
-        hello_str = "hello world %s" % rospy.get_time()
-        rospy.loginfo(hello_str)
-        pub.publish(hello_str)
-        rate.sleep()
+
+    rospy.Subscriber('/joint_states', String, callback)
+
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
 
 if __name__ == '__main__':
-    try:
-        record_trajectory()
-    except rospy.ROSInterruptException:
-        pass
+    listener()
